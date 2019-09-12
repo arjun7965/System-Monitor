e0c018ddb61cb00ed6811f0db90c102ece0d1337
Arjun Vinod <arjun7965@gmail.com>
2019-09-12 16:02:12 -0400

    System: return operating system name

--- src/system.cpp
+++ src/system.cpp
@@ -31,8 +31,13 @@ std::string System::Kernel() {
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// Return the operating system name
+std::string System::OperatingSystem() {
+    if(OS.empty()) {
+        OS = LinuxParser::OperatingSystem();
+    }
+    return OS;
+}
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
