8943104ae246d3af398458fc57aaecfd2140ff4e
Arjun Vinod <arjun7965@gmail.com>
2019-09-12 18:43:50 -0400

    System: return the total number of processes

--- src/system.cpp
+++ src/system.cpp
@@ -44,8 +44,10 @@ std::string System::OperatingSystem() {
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
 
-// TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+// Return the total number of processes on the system
+int System::TotalProcesses() {
+  return LinuxParser::TotalProcesses();
+}
 
 // TODO: Return the number of seconds since the system started running
 long int System::UpTime() { return 0; }
