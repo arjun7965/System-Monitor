704208558e5d1aceb32cf8fa7fbd62074ce716f4
Arjun Vinod <arjun7965@gmail.com>
2019-09-13 19:02:22 -0400

    System: return the system uptime

--- src/system.cpp
+++ src/system.cpp
@@ -51,5 +51,7 @@ int System::TotalProcesses() {
   return LinuxParser::TotalProcesses();
 }
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// Return the number of seconds since the system started running
+long int System::UpTime() {
+  return LinuxParser::UpTime();
+}
