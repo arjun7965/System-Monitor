be05e7ebce49d4eacc40ca3ed0d8d3965cf1091e
Arjun Vinod <arjun7965@gmail.com>
2019-09-13 19:08:10 -0400

    Format: convert the system uptime to HH:MM:SS format

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,21 @@
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long time) {
+  int day, hours, minutes, seconds;
+  string uptime;
+
+  // calculate day, hours, minutes and seconds
+  // from the time (in secs) received from /proc/uptime
+  day = (((time / 60) / 60) / 24);
+  hours = (((time / 60) / 60) % 24);
+  minutes = ((time / 60) % 60);
+  seconds = time % 60;
+
+  uptime = std::to_string(hours) + ":" + std::to_string(minutes) + ":"
+           + std::to_string(seconds) + " up " + std::to_string(day) + " day";
+
+  return uptime;
+}
