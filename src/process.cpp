d924d8dae55cecc26c616ca04298c99bd31cda5f
Arjun Vinod <arjun7965@gmail.com>
2019-09-17 18:31:43 -0400

    Process: return the command that kicked off the process

--- src/process.cpp
+++ src/process.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
@@ -21,8 +22,10 @@ float Process::CpuUtilization() const {
 // TODO: Return this process's CPU utilization
 void Process::CpuUtilization(long active_ticks, long system_ticks) { }
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// Return the command that generated this process
+string Process::Command() {
+  return LinuxParser::Command(Pid());
+}
 
 // TODO: Return this process's memory utilization
 string Process::Ram() { return string(); }
