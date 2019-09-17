57b84ae68c73130fdd8e5c6320716b81572bb0b9
Arjun Vinod <arjun7965@gmail.com>
2019-09-17 11:15:48 -0400

    Processor: return more accurate CPU utilization
    This is done by measuring difference in utilization between two points
    in time relatively close to the present

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,11 +1,17 @@
 #include "processor.h"
 #include "linux_parser.h"
 
+
 // Return the aggregate CPU utilization
 float Processor::Utilization() {
+  float cpu_utilization;
   long active_time = LinuxParser::ActiveJiffies();
   long idle_time = LinuxParser::IdleJiffies();
-  long total_time = active_time + idle_time;
-  float cpu_utilization = static_cast<float> (active_time / total_time * 100);
+  long a_time = active_time - cached_active_time;
+  long i_time = idle_time - cached_idle_time;
+  long total_time = a_time + i_time;
+  cpu_utilization = (float) a_time / (float) total_time;
+  cached_active_time = active_time;
+  cached_idle_time = idle_time;
   return cpu_utilization;
 }
