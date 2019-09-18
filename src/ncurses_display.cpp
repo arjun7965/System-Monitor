5da0eb0c5a540f9b4ba12fa7904ba5a35dfc2503
Arjun Vinod <arjun7965@gmail.com>
2019-09-18 11:03:02 -0400

    Ncurses Display: use Format::Time for process's uptime

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -76,7 +76,7 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
     mvwprintw(window, row, cpu_column, to_string(cpu).substr(0, 4).c_str());
     mvwprintw(window, row, ram_column, processes[i].Ram().c_str());
     mvwprintw(window, row, time_column,
-              Format::ElapsedTime(processes[i].UpTime()).c_str());
+              Format::Time(processes[i].UpTime()).c_str());
     mvwprintw(window, row, command_column,
               processes[i].Command().substr(0, window->_maxx - 46).c_str());
   }
