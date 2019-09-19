623db97947096d93c049199f85de0237f71591fe
Arjun Vinod <arjun7965@gmail.com>
2019-09-19 10:27:59 -0400

    Processor: measure difference in CPU utilization over 100ms to get more accurate value

--- include/processor.h
+++ include/processor.h
@@ -1,11 +1,16 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <chrono>
+#include <iostream>
+
 class Processor {
  public:
   float Utilization();                  // Return the CPU utilization of the system
 
  private:
+  int counter{0};
+  std::chrono::steady_clock::time_point time_ms;
   long cached_active_time{0};
   long cached_idle_time{0};
 };
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,17 +1,27 @@
 #include "processor.h"
 #include "linux_parser.h"
 
+using namespace std::chrono;
 
 // Return the CPU utilization
 float Processor::Utilization() {
+  if (counter == 0) {
+    time_ms = steady_clock::now();
+    counter++;
+  }
   float cpu_utilization;
   long active_time = LinuxParser::ActiveJiffies();
   long idle_time = LinuxParser::IdleJiffies();
   long a_time = active_time - cached_active_time;
   long i_time = idle_time - cached_idle_time;
   long total_time = a_time + i_time;
-  cpu_utilization = (float) a_time / (float) total_time;
-  cached_active_time = active_time;
-  cached_idle_time = idle_time;
+  auto time_diff = duration_cast<std::chrono::milliseconds>(steady_clock::now() - time_ms);
+  if (time_diff.count() > 100U) {
+    cpu_utilization = (float) a_time / (float) total_time;
+    cached_active_time = active_time;
+    cached_idle_time = idle_time;
+    time_ms = steady_clock::now();
+    return cpu_utilization;
+  }
   return cpu_utilization;
 }
